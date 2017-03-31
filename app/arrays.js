2d53c911d9a79d6e82cb00dd1111f7e5d8557136
Michael A Williamson <maw+github@williamsonclan.us>
2017-03-31 16:45:56 -0600

    My answers to arrays

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,184 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+	indexOf: function (arr, item) {
+		if (arr === null || typeof arr === "undefined"){
+			return -1;
+		}
+		if (Array.prototype.indexOf){
+			return arr.indexOf(item);
+		}
+		else{
+			for(let i=0;i<arr.length;i++){
+				if (arr[i] === item){
+					return i;
+				}
+			}
+		}
+		return -1;
+	},
 
-  },
+	sum: function (arr) {
+		if (arr === null || typeof arr === "undefined"){
+			return -1;
+		}
 
-  sum: function(arr) {
+		let sum = 0;
 
-  },
+		for(let i=0;i<arr.length;i++){
+			sum += arr[i];
+		}
+		return sum;
+	},
 
-  remove: function(arr, item) {
+	remove: function (arr, item) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  },
+		let newarray = [];
 
-  removeWithoutCopy: function(arr, item) {
+		for(let i=0;i<arr.length;i++){
+			if (arr[i] !== item){
+				newarray.push(arr[i]);
+			}
+		}
 
-  },
+		return newarray;
+	},
 
-  append: function(arr, item) {
+	removeWithoutCopy: function (arr, item) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  },
+		for(let i=arr.length;i>=0;i--){
+			if (arr[i] === item){
+				arr.splice(i, 1);
+			}
+		}
 
-  truncate: function(arr) {
+		return arr;
+	},
 
-  },
+	append: function (arr, item) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  prepend: function(arr, item) {
+		arr.push(item);
+		return arr;
+	},
 
-  },
+	truncate: function (arr) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  curtail: function(arr) {
+		arr.pop();
+		return arr;
+	},
 
-  },
+	prepend: function (arr, item) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  concat: function(arr1, arr2) {
+		arr.splice(0, 0, item);
 
-  },
+		return arr;
+	},
 
-  insert: function(arr, item, index) {
+	curtail: function (arr) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  },
+		arr.splice(0, 1);
 
-  count: function(arr, item) {
+		return arr;
+	},
 
-  },
+	concat: function (arr1, arr2) {
+		if (arr1 === null || typeof arr1 === "undefined" || arr2 === null || typeof arr2 === "undefined"){
+			return -1;
+		}
+		if (Array.prototype.concat){
+			return arr1.concat(arr2);
+		}
+		else{
+			for(let i=0;i<arr2.length;i++){
+				arr1.push(arr2);
+			}
+		}
+		return arr1;
+	},
 
-  duplicates: function(arr) {
+	insert: function (arr, item, index) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  },
+		arr.splice(index, 0, item);
 
-  square: function(arr) {
+		return arr;
+	},
 
-  },
+	count: function (arr, item) {
+		if (arr === null || typeof arr === "undefined"){
+			return;
+		}
 
-  findAllOccurrences: function(arr, target) {
+		let count = arr.reduce((acc, val)=>{
+			if (val === item){
+				acc++;
+			}
+			return acc;
+		});
 
-  }
+		return count -1;
+	},
+
+	duplicates: function (arr) {
+		if (arr === null || typeof arr === "undefined"){
+			return [];
+		}
+
+		let dups = [];
+
+		for (let i=0;i<arr.length;i++){
+			if (arr.lastIndexOf(arr[i]) !== i && !dups.includes(arr[i])){
+				dups.push(arr[i]);
+			}
+		}
+
+		return dups;
+	},
+
+	square: function (arr) {
+		if (arr === null || typeof arr === "undefined"){
+			return [];
+		}
+
+		for (let i=0;i<arr.length;i++){
+			arr[i] = arr[i] * arr[i];
+		}
+		return arr;
+	},
+
+	findAllOccurrences: function (arr, target) {
+		if (arr === null || typeof arr === "undefined"){
+			return [];
+		}
+
+		let occurances = [];
+
+		let marker = arr.indexOf(target);
+		while (marker != -1){
+			occurances.push(marker);
+			marker = arr.indexOf(target, marker+1);
+		}
+
+		return occurances;
+	}
 };
