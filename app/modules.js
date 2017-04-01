149dcc206d2c63bf6f8ad9eeb1252b82f0ded51e
Michael A Williamson <maw+github@williamsonclan.us>
2017-04-01 17:36:08 -0600

    operators, modules, and numbers, yay

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,16 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
-  createModule: function(str1, str2) {
+	createModule: function (str1, str2) {
 
-  }
+		let mod = function(g,n){
+			this.name = n;
+			this.greeting = g;
+			this.sayIt = function () {
+				return this.greeting + ", " + this.name;
+			}
+		};
+
+		return new mod(str1, str2);
+	}
 };
--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,32 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
+	valueAtBit: function (num, bit) {
+		let mask = 1 << (bit-1);
+		return (num & mask)?1:0;
 
-  },
+	},
 
-  base10: function(str) {
+	base10: function (str) {
+		return parseInt(str, 2);
+	},
 
-  },
+	convertToBinary: function (num) {
+		let n = Number(num).toString(2);
+		return "00000000".substr(n.length)+n;
+	},
 
-  convertToBinary: function(num) {
+	multiply: function (a, b) {
 
-  },
+		function decimalPlaces(number) {
+			if(Math.floor(number.valueOf()) === number.valueOf()) return 0;
+			return number.toString().split(".")[1].length || 0;
+		}
 
-  multiply: function(a, b) {
+		let adec = decimalPlaces(a);
+		let bdec = decimalPlaces(b);
+		let dec = Math.max(adec, bdec);
 
-  }
+		return parseFloat((a * b).toFixed(dec));
+	}
 };
