191acb9593efdf912893b1a1d9be3dc2dcad82f6
Michael A Williamson <maw+github@williamsonclan.us>
2017-04-01 11:44:43 -0600

    Upgrading packages to be more modern

--- app/async.js
+++ app/async.js
@@ -1,11 +1,21 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+	async: function (value) {
+		return new Promise((res,rej)=>{
+			res(value);
+		});
+	},
 
-  },
-
-  manipulateRemoteData: function(url) {
-
-  }
+	manipulateRemoteData: function (url) {
+		return new Promise((res,rej)=>{
+			let _c = require(url);
+			if (!_c){
+				rej("Could not load " + url);
+			}
+			else{
+				res(JSON.parse(_c));
+			}
+		});
+	}
 };
