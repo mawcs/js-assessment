6dd6d31d1c706c1795c0ddd0cdbfdd34b3427fee
Michael A Williamson <maw+github@williamsonclan.us>
2017-04-01 14:46:39 -0600

    functions... ugh.

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,73 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function(fn, arr) {
+	argsAsArray: function (fn, arr) {
+		return fn(arr[0], arr[1], arr[2]);
+	},
 
-  },
+	speak: function (fn, obj) {
+		return fn.apply(obj, null);
+	},
 
-  speak: function(fn, obj) {
+	functionFunction: function (str) {
+		return (place) => {return str + ", " + place};
+	},
 
-  },
+	makeClosures: function (arr, fn) {
+		let retval = [];
 
-  functionFunction: function(str) {
+		let _fn = (val) => {
+			return () => {return fn(val)};
+		};
 
-  },
+		for (let i=0;i<arr.length;i++){
+			retval.push(_fn(arr[i]));
+		}
 
-  makeClosures: function(arr, fn) {
+		return retval;
+	},
 
-  },
+	partial: function (fn, str1, str2) {
+		return (part) => {
+			return fn.apply(null, [str1, str2, part]);
+		};
+	},
 
-  partial: function(fn, str1, str2) {
+	useArguments: function () {
+		let retval = 0;
+		for (let i=0;i<arguments.length;i++){
+			if (!isNaN(arguments[i])){
+				retval += arguments[i];
+			}
+		}
+		return retval;
+	},
 
-  },
+	callIt: function (fn) {
+		let args = Array.from(arguments).slice(1,arguments.length);
+		return fn.apply(null, args);
+	},
 
-  useArguments: function() {
+	partialUsingArguments: function (fn) {
+		let args = Array.from(arguments).slice(1,arguments.length);
 
-  },
+		return function (){
+			let completeargs = args.concat(Array.from(arguments));
+			return fn.apply(null, completeargs);
+		};
+	},
 
-  callIt: function(fn) {
-
-  },
-
-  partialUsingArguments: function(fn) {
-
-  },
-
-  curryIt: function(fn) {
-
-  }
+	curryIt: function (fn) {
+		let currier = function(next, total){
+			return (current)=>{
+				if (next.push(current) < total){
+					return currier(next, total);
+				}
+				else{
+					return fn.apply(null, next);
+				}
+			}
+		};
+		return currier([], fn.length);
+	}
 };
