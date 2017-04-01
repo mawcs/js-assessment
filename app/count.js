945b19cec6583546ce43607faaeddbba409196b3
Michael A Williamson <maw+github@williamsonclan.us>
2017-04-01 12:42:06 -0600

    done with kludgey count

--- app/count.js
+++ app/count.js
@@ -1,7 +1,26 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
 
-  }
+	count: function (start, end) {
+		let _self = this;
+		let iid;
+
+		function doCount() {
+			console.log(start++);
+
+			if (start <= end) {
+				iid = setTimeout(doCount, 100);
+			}
+		}
+
+		doCount();
+
+		return {
+			cancel: function () {
+				iid && clearTimeout(iid);
+			}
+		};
+	}
+
 };
