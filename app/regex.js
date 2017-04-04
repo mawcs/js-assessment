a922f970a79b9221aaa6d8d1360f011fa83c9af1
Michael A Williamson <maw+github@williamsonclan.us>
2017-04-04 10:17:48 -0600

    regex

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,32 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
-  containsNumber: function(str) {
+	containsNumber: function (str) {
+		return /\d+/.test(str);
+	},
 
-  },
+	containsRepeatingLetter: function (str) {
+		return /(\D)\1{1,}/.test(str);
+	},
 
-  containsRepeatingLetter: function(str) {
+	endsWithVowel: function (str) {
+		return /^.*[aeiouAEIOU]$/.test(str);
+	},
 
-  },
+	captureThreeNumbers: function (str) {
+		let reg = /(\d{3})/;
+		let _m = str.match(reg);
+		if (_m && _m.length > 0){
+			return _m[0];
+		}
+		return false;
+	},
 
-  endsWithVowel: function(str) {
+	matchesPattern: function (str) {
+		return /^\d{3}-\d{3}-\d{4}$/.test(str);
+	},
 
-  },
-
-  captureThreeNumbers: function(str) {
-
-  },
-
-  matchesPattern: function(str) {
-
-  },
-
-  isUSD: function(str) {
-
-  }
+	isUSD: function (str) {
+		return /^[\$]{1}[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$/.test(str);
+	}
 };
