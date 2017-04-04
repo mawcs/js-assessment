5475ea52ce2b52e3c6c3e2072e05fcd42de4d9cd
Michael A Williamson <maw+github@williamsonclan.us>
2017-04-04 11:19:53 -0600

    strings

--- app/strings.js
+++ app/strings.js
@@ -1,15 +1,56 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
+	reduceString: function (str, amount) {
+		let retval = [];
+		let curchar = 0;
+		let curcount = 0;
+		for (let i=0;i<str.length;i++){
+			let _c = str.charAt(i);
+			if (_c === curchar){
+				curcount++;
+				if (curcount < amount){
+					retval.push(curchar);
+				}
+			}
+			else{
+				curchar = _c;
+				curcount = 0;
+				retval.push(curchar);
+			}
+		}
+		return retval.join("");
+	},
 
-  },
+	wordWrap: function (str, cols) {
+		let retval = [];
+		let _split = str.split(" ");
 
-  wordWrap: function(str, cols) {
+		let i=0;
+		let _line = "";
 
-  },
+		while (i<_split.length){
+			_line = "";
+			_line += _split[i++];
+			while (_line.length < cols && i<_split.length-1){
+				_line += " ";
+				if (_line.length >= cols) break;
+				if ((_line + _split[i]).length > cols){
+					break;
+				}
+				_line += _split[i++];
+			}
+			_line = _line.trim();
+			_line += "\n";
+			retval.push(_line);
+		}
 
-  reverseString: function(str) {
+		return retval.join("").trim();
+	},
 
-  }
+	reverseString: function (str) {
+		let _a = str.split("");
+		_a = _a.reverse();
+		return _a.join("");
+	}
 };
